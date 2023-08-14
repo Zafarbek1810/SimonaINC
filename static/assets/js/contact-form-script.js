4c1ddb496ffc6b437c5afb131e5e2da26611428a
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-08-14 22:37:15 +0500

    form changed

--- static/assets/js/contact-form-script.js
+++ static/assets/js/contact-form-script.js
@@ -20,15 +20,15 @@
         // Initiate Variables With Form Content
         var name = $("#name").val();
         var email = $("#email").val();
-        var msg_subject = $("#msg_subject").val();
-        var phone_number = $("#phone_number").val();
-        var message = $("#message").val();
+        var subject = $("#msg_subject").val();
+        var phone = $("#phone_number").val();
+        var description = $("#message").val();
 
 
         $.ajax({
             type: "POST",
             url: "assets/php/form-process.php",
-            data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&phone_number=" + phone_number + "&message=" + message,
+            data: "name=" + name + "&email=" + email + "&msg_subject=" + subject + "&phone_number=" + phone + "&message=" + description,
             success : function(text){
                 if (text == "success"){
                     formSuccess();
